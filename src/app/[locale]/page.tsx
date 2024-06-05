570f086f6705b4bf908891b5086163e817cfcede
Yiming-Liao <yimingliao.official@gmail.com>
2024-06-05 21:24:46 +0800

    height

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -18,7 +18,7 @@ const Home: NextPage = () => {
             <div className="max-sm:mt-28 mt-36 mb-12">
                 <h1 className={`${fontClassName} font-semibold text-3xl text-slate-700`}>🖼️ {t('title')}</h1>
             </div>
-            <div className="max-w-96 w-[90%] min-h-[450px] shadow-xl rounded-xl border-4 border-gray-200/50">
+            <div className="max-w-96 w-[90%] h-[480px] max-sm:h-[453px] shadow-xl rounded-xl border-4 border-gray-200/50">
                 <ImageCompressor />
             </div>
         </main>
