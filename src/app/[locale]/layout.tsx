c0f25b900bd5b80047a51c4dad2a2ce0b6a70b98
Yiming-Liao <yimingliao.official@gmail.com>
2024-06-06 11:19:43 +0800

    2

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata({ params }: { params: { locale: string } 
                 rel: 'apple-touch-icon',
                 type: 'image/png',
                 sizes: '180x180',
-                url: '/apple-icon.png',
+                url: 'http:192.168.1.211:3000/en/apple-icon.png',
             },
         ],
     };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,20 +4,6 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Image Compressor",
   description: "A tool to compress images with ease.",
-  icons: [
-    {
-      rel: 'icon',
-      type: 'image/png',
-      sizes: '16x16',
-      url: '/icon.png',
-    },
-    {
-      rel: 'apple-touch-icon',
-      type: 'image/png',
-      sizes: '180x180',
-      url: '/apple-icon.png',
-    },
-  ],
 }
 
 export default function RootLayout({
